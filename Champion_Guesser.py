feff41ff2c6eb01f0b9ef30fe88af4e9b97d6c7d
RitterAntoine <antoine.ritter@viacesi.fr>
2024-04-02 12:32:51 +0200

    feat: better guessing algorithm

--- Champion_Guesser.py
+++ Champion_Guesser.py
@@ -38,17 +38,56 @@ def filter_champions(champions, feedback, guessed_champion):
             champions = [champion for champion in champions if champion[field] > guessed_champion[field]]
         elif feedback[field] == 'lower':
             champions = [champion for champion in champions if champion[field] < guessed_champion[field]]
+        elif feedback[field] == 'partial':
+            champions = [champion for champion in champions if guessed_champion[field] in champion[field]]
     return champions
+
+# Initialize scores
+def initialize_scores(champions):
+    return {champion['Champion']: 0 for champion in champions}
+
+# Update scores based on user feedback
+def update_scores(scores, champions, feedback, guessed_champion):
+    for field in feedback:
+        if feedback[field] == 'yes':
+            for champion in champions:
+                if champion[field] == guessed_champion[field]:
+                    scores[champion['Champion']] += 1
+        elif feedback[field] == 'no':
+            for champion in champions:
+                if champion[field] == guessed_champion[field]:
+                    scores[champion['Champion']] -= 1
+        elif feedback[field] == 'higher':
+            for champion in champions:
+                if champion[field] > guessed_champion[field]:
+                    scores[champion['Champion']] += 1
+        elif feedback[field] == 'lower':
+            for champion in champions:
+                if champion[field] < guessed_champion[field]:
+                    scores[champion['Champion']] += 1
+        elif feedback[field] == 'partial':
+            for champion in champions:
+                if guessed_champion[field] in champion[field]:
+                    scores[champion['Champion']] += 0.5
+    return scores
+
+# Choose the most probable champion
+def choose_most_probable_champion(champions, scores):
+    max_score = max(scores.values())
+    most_probable_champions = [champion for champion in champions if scores[champion['Champion']] == max_score]
+    return random.choice(most_probable_champions)
 
 # Main function
 def main():
     champions = load_champions()
+    scores = initialize_scores(champions)
     while len(champions) > 1:
-        guessed_champion = choose_random_champion(champions)
+        guessed_champion = choose_most_probable_champion(champions, scores)
         feedback = get_user_feedback(guessed_champion)
         if feedback is True:
             print(f"The champion is: {guessed_champion['Champion']}")
             return
+        scores = update_scores(scores, champions, feedback, guessed_champion)
         champions = filter_champions(champions, feedback, guessed_champion)
     if champions:
         print(f"The champion is: {champions[0]['Champion']}")
