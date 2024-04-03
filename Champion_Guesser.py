61bdf3a421911a1b7db03c1e5e0eb503c9e32c44
RitterAntoine <antoine.ritter@viacesi.fr>
2024-04-03 10:41:57 +0200

    feat: Partial answer fixed, and user answer reduced

--- Champion_Guesser.py
+++ Champion_Guesser.py
@@ -13,33 +13,35 @@ def choose_random_champion(champions):
 
 # Get user feedback on the champion
 def get_user_feedback(champion):
-    is_correct = input(f"Is the champion {champion['Champion']}? (yes/no): ")
-    if is_correct.lower() == 'yes':
-        return True
     feedback = {}
     for field in champion:
         if field == "Champion":
-            continue
-        value = ', '.join(champion[field]) if isinstance(champion[field], list) else champion[field]
-        if field == "Release year":
-            feedback[field] = input(f"Is the {field} ({value}) correct for {champion['Champion']}? (yes/higher/lower): ")
+            is_correct = input(f"Is the champion {champion['Champion']}? (y for yes/n for no): ")
+            if is_correct.lower() == 'y':
+                return True
+            else:
+                feedback[field] = 'n'
         else:
-            feedback[field] = input(f"Is the {field} ({value}) correct for {champion['Champion']}? (yes/no/partial): ")
+            value = ', '.join(champion[field]) if isinstance(champion[field], list) else champion[field]
+            if field == "Release year":
+                feedback[field] = input(f"Is the {field} ({value}) correct for {champion['Champion']}? (y for yes/h for higher/l for lower): ")
+            else:
+                feedback[field] = input(f"Is the {field} ({value}) correct for {champion['Champion']}? (y for yes/n for no/p for partial): ")
     return feedback
 
 # Filter champions based on user feedback
 def filter_champions(champions, feedback, guessed_champion):
     for field in feedback:
-        if feedback[field] == 'yes':
+        if feedback[field] == 'y':
             champions = [champion for champion in champions if champion[field] == guessed_champion[field]]
-        elif feedback[field] == 'no':
+        elif feedback[field] == 'n':
             champions = [champion for champion in champions if champion[field] != guessed_champion[field]]
-        elif feedback[field] == 'higher':
+        elif feedback[field] == 'h':
             champions = [champion for champion in champions if champion[field] > guessed_champion[field]]
-        elif feedback[field] == 'lower':
+        elif feedback[field] == 'l':
             champions = [champion for champion in champions if champion[field] < guessed_champion[field]]
-        elif feedback[field] == 'partial':
-            champions = [champion for champion in champions if guessed_champion[field] in champion[field]]
+        elif feedback[field] == 'p':
+            champions = [champion for champion in champions if set(guessed_champion[field]).intersection(champion[field])]
     return champions
 
 # Initialize scores
@@ -49,25 +51,25 @@ def initialize_scores(champions):
 # Update scores based on user feedback
 def update_scores(scores, champions, feedback, guessed_champion):
     for field in feedback:
-        if feedback[field] == 'yes':
+        if feedback[field] == 'y':
             for champion in champions:
                 if champion[field] == guessed_champion[field]:
                     scores[champion['Champion']] += 1
-        elif feedback[field] == 'no':
+        elif feedback[field] == 'n':
             for champion in champions:
                 if champion[field] == guessed_champion[field]:
                     scores[champion['Champion']] -= 1
-        elif feedback[field] == 'higher':
+        elif feedback[field] == 'h':
             for champion in champions:
                 if champion[field] > guessed_champion[field]:
                     scores[champion['Champion']] += 1
-        elif feedback[field] == 'lower':
+        elif feedback[field] == 'l':
             for champion in champions:
                 if champion[field] < guessed_champion[field]:
                     scores[champion['Champion']] += 1
-        elif feedback[field] == 'partial':
+        elif feedback[field] == 'p':
             for champion in champions:
-                if guessed_champion[field] in champion[field]:
+                if set(guessed_champion[field]).intersection(champion[field]):
                     scores[champion['Champion']] += 0.5
     return scores
 
